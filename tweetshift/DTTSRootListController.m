2105716a704cec3380aa5725a8b7f855ac30bb90
Dave1482 <thedave1482@gmail.com>
2020-06-02 11:56:36 -0500

    v0.0.2-beta2006021055

--- tweetshift/DTTSRootListController.m
+++ tweetshift/DTTSRootListController.m
@@ -1,6 +1,35 @@
 #include "DTTSRootListController.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <spawn.h>
+#include <dlfcn.h>
 
 @implementation DTTSRootListController
+
+#define _POSIX_SPAWN_DISABLE_ASLR 0x0100
+#define _POSIX_SPAWN_ALLOW_DATA_EXEC 0x2000
+extern char **environ;
+
+void run_cmd(char *cmd)
+{
+    
+    pid_t pid;
+    char *argv[] = {"sh", "-c", cmd, NULL};
+    int status;
+    //printf("Run command: %s\n", cmd);
+    status = posix_spawn(&pid, "/bin/sh", NULL, NULL, (char* const*)argv, environ);
+    
+    if (status == 0) {
+        printf("Child pid: %i\n", pid);
+        if (waitpid(pid, &status, 0) != -1) {
+            printf("Child exited with status %i\n", status);
+        } else {
+            perror("waitpid");
+        }
+    } else {
+        printf("posix_spawn: %s\n", strerror(status));
+    }
+}
 
 - (NSArray *)specifiers {
 	if (!_specifiers) {
@@ -10,7 +39,7 @@
 	return _specifiers;
 }
 -(void)respring {
-         [(SpringBoard *)[UIApplication sharedApplication] _relaunchSpringBoardNow];
+         run_cmd("killall -9 SpringBoard");
       }
 
 @end
